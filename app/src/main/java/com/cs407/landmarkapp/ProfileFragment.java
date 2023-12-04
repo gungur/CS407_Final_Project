44c6ed53ceb5349cc2a6228b7a1df213246f609d
Daniel Ocampo <danielocampo182@gmail.com>
2023-12-04 12:33:45 -0600

    Added basic profile fragment layout with current available badges. Adjusted size of union south badge

--- app/src/main/java/com/cs407/landmarkapp/ProfileFragment.java
+++ app/src/main/java/com/cs407/landmarkapp/ProfileFragment.java
@@ -1,18 +1,27 @@
 package com.cs407.landmarkapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 // TODO: implement functions and stuff for the fragment
 public class ProfileFragment extends Fragment {
-
+    AppDatabase appDatabase;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -28,6 +37,35 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        appDatabase = AppDatabase.getInstance(this.getContext());
+
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        displayUsername();
+    }
+
+    private void displayUsername() {
+
+        appDatabase.userDao().getUserById(getUserIdFromPrefs()).observe(getViewLifecycleOwner(), new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                if (user == null) return;
+
+                TextView usernameText = getView().findViewById(R.id.usernameText);
+                usernameText.setText(user.getUsername());
+
+            }
+        });
+    }
+
+
+
+    private int getUserIdFromPrefs() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        return sharedPreferences.getInt("userId", -1);
     }
 }
