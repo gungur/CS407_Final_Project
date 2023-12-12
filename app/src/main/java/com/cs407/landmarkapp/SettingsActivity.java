34dcfb04e9cf57a141a8f3a2af39548d840ba1af
a <amoudy@wisc.edu>
2023-12-12 03:25:44 -0600

    Fixed settings display and added auto login/login remember on the start screen

--- app/src/main/java/com/cs407/landmarkapp/SettingsActivity.java
+++ app/src/main/java/com/cs407/landmarkapp/SettingsActivity.java
@@ -1,11 +1,15 @@
 package com.cs407.landmarkapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Debug;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,17 +32,29 @@ public class SettingsActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("userInfo",MODE_PRIVATE);
         int userId = sharedPreferences.getInt("userId",-1);
 
+            appDatabase.userDao().getUserById(userId).observeForever(new Observer<User>() {
+                @Override
+                public void onChanged(User user) {
+                    usernameText.setText(user.getUsername());
+                    emailText.setText(user.getEmail());
+                }
+            });
 
-        if(userId != -1){
-            user = appDatabase.userDao().getUserById(userId).getValue();
-        }
+        Button logout = findViewById(R.id.logoutBtn);
 
-        if(user != null) {
-            usernameText.setText(user.getUsername());
-            emailText.setText(user.getEmail());
-        }else{
-            Toast toast = Toast.makeText(this,"Error Loading Data", Toast.LENGTH_LONG);
-            toast.show();
-        }
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putInt("userId",-1);
+                editor.putString("username",null);
+                editor.putString("password",null);
+                editor.commit();
+
+                Intent intent = new Intent(SettingsActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
